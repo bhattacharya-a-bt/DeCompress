836a3f0bf0687868d3e2833aaa0a4399a44eba0b
Arjun Bhattacharya <abhattac@live.unc.edu>
2020-04-13 15:03:44 -0400

    magic cs

---
+++ R/l2Magic.R
@@ -0,0 +1,41 @@
+l2Magic <- function(need,
+                    train,
+                    seed = 1218){
+
+    set.seed(seed)
+    Tt = diag(nrow(train))
+    p = matrix(0,nrow(train),1)
+
+    mod = R1magic::solveL1(t(train),
+                           need,
+                           Tt,
+                           p)
+
+    ### DO CV
+    pred = vector('numeric',
+                  length(need))
+    train.folds = caret::createFolds(y = need,
+                                     k=5,
+                                     returnTrain = T)
+
+    for (i in 1:length(train.folds)){
+
+        new.train = train[,train.folds[[i]]]
+        Tt = diag(nrow(new.train))
+        p = matrix(0,nrow(new.train),1)
+        new.mod = R1magic::solveL2(t(new.train),
+                                   need[train.folds[[i]]],
+                                   Tt,
+                                   p)
+        pred[-train.folds[[i]]] = new.mod$estimate %*% train[,
+                                                             -train.folds[[i]]]
+
+
+    }
+
+
+    return(list(coef = mod$estimate,
+                r2 = cor(need,pred)^2))
+
+}
+
