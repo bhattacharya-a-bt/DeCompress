2605dd2b30b19b02740f1750a50a872aebbb1e09
Arjun Bhattacharya <abhattac@live.unc.edu>
2020-04-13 19:25:53 -0400

    parallel

--- R/trainCS.R
+++ R/trainCS.R
@@ -7,7 +7,9 @@ trainCS <- function(yref,
                                'ridge',
                                'l1',
                                'TV',
-                               'l2')){
+                               'l2'),
+                    par = T,
+                    n.cores){
 
     if (class(yref) != 'matrix'){
         stop("matrix not supplied in yref")
@@ -17,12 +19,27 @@ trainCS <- function(yref,
         stop("matrix not supplied in yref_need")
     }
 
+    if (!par){
     compression = pbapply::pbapply(yref_need,
                                    MARGIN = 1,
                                    trainCS_gene,
                                    train = yref,
                                    seed=seed,
                                    method = method)
+    }
+
+    if (par){
+        yref_need_list = split(yref_need,
+                               rep(1:nrow(yref_need),
+                                   each = ncol(yref_need)))
+        compression = parallel::mclapply(yref_need_list,
+                                         trainCS_gene,
+                                         train = yref,
+                                         seed=seed,
+                                         method = method,
+                                         mc.cores = n.cores)
+    }
+
     compression_mat = sapply(compression,function(x) x$coef)
     r2Vec = sapply(compression,function(x) x$r2)
 
