4aec0c6730e90b665ae627a504148a4de96fd82d
Arjun Bhattacharya <abhattac@live.unc.edu>
2020-04-13 16:17:23 -0400

    enet output

--- R/enet.R
+++ R/enet.R
@@ -4,17 +4,12 @@ enet <- function(need,
                  alpha){
 
     set.seed(seed)
-    lar = tryCatch(glmnet::cv.glmnet(x = t(train),
-                                     y = need,
-                                     alpha = alpha,
-                                     k = 5),
-                   error = function(e) {return(NULL)})
-    if (is.null(lar)){
-        lar.coef = NULL
-        lar = list(cv = var(need))
-        }
+    lar = glmnet::cv.glmnet(x = t(train),
+                            y = need,
+                            alpha = alpha,
+                            k = 5)
 
-    return(list(coef = lar.coef,
+    return(list(coef = as.numeric(coef(lar,s='lambda.min')[-1,1]),
                 r2 = lar$glmnet.fit$dev.ratio[which(lar$glmnet.fit$lambda ==
                                                         lar$lambda.min)]))
 
