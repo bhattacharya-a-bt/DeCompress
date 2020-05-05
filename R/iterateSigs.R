33ad27977c8ccb757293f72b64573cc22e3912dc
Arjun Bhattacharya <abhattac@live.unc.edu>
2020-05-05 14:49:47 -0400

    findNumberCells init

--- R/iterateSigs.R
+++ R/iterateSigs.R
@@ -2,34 +2,24 @@ iterateSigs <- function(yref,
                         n.types = NULL,
                         n.markers = 1000,
                         n.iter = 30,
-                        scree = c('drop','cumvar','residual')){
+                        scree = c('drop','cumvar','residual'),
+                        log = T){
 
 
     if (class(yref) != c('matrix')){
         stop("matrix not supplied in yref")
     }
 
-    if (is.null(n.types)){
-        s = svd(yref)
-        if (scree == 'residual'){
-            p = 10
-            y <- lapply(1:(p-1),function(i)
-                try(structureMeasure(s,lo$exp$filtered$norm,i)))
-            y2 <- unlist(y[sapply(y,
-                                  function(x) !inherits(x, "try-error"))])
-            y2.ind <- which(sapply(y,
-                                   function(x) !inherits(x, "try-error")))
-            n.types = y2.ind[which.min(y2)]
-        }
-        if (scree == 'cumvar'){
-            dii = s$d^2
-            cumvar = cumsum(dii/sum(dii))
-            n.types = min(which(cumvar >= .8))
-        }
-        if (scree == 'drop'){
-            n.types = kaiser(s$d^2)
-        }
+    if (!log){
+        yref = log2(yref+1)
     }
+
+    row.means = rowSums(yref)
+    yref = yref[row.means > 0,]
+
+    if (is.null(n.types)){
+        n.types = findNumberCells(yref,scree = scree)
+        }
 
     geneList = rownames(yref)
     rownames(yref) = NULL
--- R/linCor.R
+++ R/linCor.R
@@ -29,26 +29,8 @@ linCor <- function(yref,
     lo$filterDatasetByPval(pval)
 
     if (is.null(n.types)){
-        s = svd(lo$exp$filtered$norm)
-        if (scree == 'residual'){
-        p = 10
-        y <- lapply(1:(p-1),function(i)
-            try(structureMeasure(s,lo$exp$filtered$norm,i)))
-        y2 <- unlist(y[sapply(y,
-                              function(x) !inherits(x, "try-error"))])
-        y2.ind <- which(sapply(y,
-                               function(x) !inherits(x, "try-error")))
-        n.types = y2.ind[which.min(y2)]
+        n.types = findNumberCells(yref,scree = scree)
         }
-        if (scree == 'cumvar'){
-            dii = s$d^2
-            cumvar = cumsum(dii/sum(dii))
-            n.types = min(which(cumvar >= .8))
-        }
-        if (scree == 'drop'){
-            n.types = kaiser(s$d^2)
-        }
-    }
 
     lo$setCellTypeNumber(n.types)
     lo$project("full")
