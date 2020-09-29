922c028e159fea225fb75d3cda55f0b8c2b715d6
Arjun Bhattacharya <abhattac@live.unc.edu>
2020-09-28 21:10:30 -0400

    bigstats

---
+++ R/bigstatsenet.R
@@ -0,0 +1,48 @@
+#' Elastic-net for compressed sensing using bigstatsr
+#'
+#' The function runs elastic net to train compression for a single gene
+#'
+#' @param need vector, numeric vector of gene that is needed
+#' @param train matrix, numeric expression matrix of training data
+#' @param seed numeric, random seed
+#' @param alpha numeric, between 0 and 1
+#'
+#' @return list with coefficients and predictive R2
+#'
+#' @importFrom bigstatsr FBM
+#' @importFrom bigstatsr big_spLinReg
+#'
+#' @export
+bigstatsenet <- function(need,
+                 train,
+                 seed = 1218,
+                 alpha){
+
+    if (alpha < 0 | alpha > 1){
+        stop('alpha is not between 0 and 1')
+    }
+
+    backing = paste(sample(letters,10),collapse='')
+    if (file.exists(paste0(backing,'.bk'))){
+        file.remove(paste0(backing,'.bk'))
+    }
+    X = bigstatsr::FBM(nrow = nrow(train),
+            ncol = ncol(train),
+            init = as.vector(train),
+            backing = backing)
+
+    set.seed(seed)
+    lar = bigstatsr::big_spLinReg(X,
+                                  y = need,
+                                  alpha = alpha,
+                                  K = 5)
+
+
+    if (file.exists(paste0(backing,'.bk'))){
+        file.remove(paste0(backing,'.bk'))
+    }
+
+    return(list(coef = unlist(summary(lar)$beta),
+                r2 = 1 - (summary(lar)$validation_loss/var(need))))
+
+}
